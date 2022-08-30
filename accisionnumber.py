56a3f4d2d7a7d61f22a79857f84701e6b409ab10
pavanGandham44 <pavangandham44@gmail.com>
2022-08-30 20:21:06 +0530

    scrape NCBI page to get Accision Number of protien

--- accisionnumber.py
+++ accisionnumber.py
@@ -3,21 +3,21 @@ import requests
 import pandas as pa
 url = requests.get('https://www.ncbi.nlm.nih.gov/protein/?term=keratin')
 soup = BeautifulSoup(url.text,'html5lib')
+names = []
+numberOfProtiens = []
+number = []
 
 beautiful = soup.find_all(class_='rslt')
-names = []
 for name in beautiful:
     names.append(name.a.get_text())
 
 
 gang = soup.find_all(class_='supp')
-numberOfProtiens = []
 for num in gang:
     numberOfProtiens.append(num.p.get_text())
 
 
 accisionNumber = soup.find_all(class_='resc')
-number = []
 for acc in accisionNumber:
    number.append(acc.find('dd').get_text())
 
